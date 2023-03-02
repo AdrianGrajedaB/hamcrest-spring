351cd35021289e6da4399a3c9b30be37fb52230d
adrian.grajeda <adrian.grajeda@unosquare.com>
2023-03-01 23:10:15 -0400

    Initial commit

---
+++ src/main/java/org/adriangrajedab/demo/hamcrest/spring/hamcrestspring/HamcrestSpringApplication.java
@@ -0,0 +1,13 @@
+package org.adriangrajedab.demo.hamcrest.spring.hamcrestspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamcrestSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HamcrestSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/adriangrajedab/demo/hamcrest/spring/hamcrestspring/HamcrestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.adriangrajedab.demo.hamcrest.spring.hamcrestspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamcrestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
